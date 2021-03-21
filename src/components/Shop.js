e8251995b507505bd954c21a1cf8a7e61a0d420e
Sultan <sultanp@gmail.com>
2021-03-21 20:07:22 +0000

    updated reducer function

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -10,6 +10,21 @@ function reducer(state, action) {
         }
         return product;
       });
+    case 'decrement':
+      return state.map((product) => {
+        if (product.sku === action.payload && product.basket > 0) {
+          return { ...product, basket: product.basket - 1 };
+        }
+        return product;
+      });
+    case 'remove':
+      return state.map((product) => {
+        if (product.sku === action.payload && product.basket > 0) {
+          return { ...product, basket: 0 };
+        }
+        return product;
+      });
+
     default:
       return state;
   }
@@ -20,14 +35,27 @@ function Shop() {
 
   return (
     <div>
+      basket count:{' '}
+      {stock.reduce((a, item) => {
+        return a + item.basket;
+      }, 0)}
       Products Page
       <h1>{stock[0].name}</h1>
       <button
         type='button'
-        onClick={() => dispatch({ type: 'increment', payload: 'ps5-001' })}
+        onClick={() => dispatch({ type: 'increment', payload: stock[0].sku })}
       >
+        Increase
+      </button>
+      <p>
         count:
         {stock[0].basket}
+      </p>
+      <button
+        type='button'
+        onClick={() => dispatch({ type: 'decrement', payload: stock[0].sku })}
+      >
+        Decrease
       </button>
     </div>
   );

477c1ecf5a3a55446a9a7f578f1d20a406db82a8
Sultan <sultanp@gmail.com>
2021-04-03 23:25:39 +0100

    updated routing and package.json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename='/'>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>,
